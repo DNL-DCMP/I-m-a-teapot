9d92713fca1f4aa5700dc491abebcf0e77d80872
Ailin Sofía <afalcon@fi.uba.ar>
2024-11-29 21:00:07 -0300

    Get user/s

--- src/users.js
+++ src/users.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let users = [{
+    id: 1,
+    name: "ailin",
+    age: 20
+}]
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
 
 app.get('/api/v1/users', (req, res) => {
-    
+    res.json(users)
+})
+
+app.get('/api/v1/users/:id', (req, res) => {
+    const user = users.find((user) => user.id == req.params.id)
+
+    if (user === undefined){
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(user)
 })
 
 app.listen(port, () => {
