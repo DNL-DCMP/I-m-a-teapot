bba3de4287277333065e92edb584b7a315842e37
Ailin Sofía <afalcon@fi.uba.ar>
2024-12-13 19:53:01 -0300

    Agrego funcionalidad a los buttons de crear receta

--- frontend/create-recipe.js
+++ frontend/create-recipe.js
@@ -25,3 +25,44 @@ navOpenBtn.addEventListener('click', () => {
 navCloseBtn.addEventListener('click', () => {
     nav.classList.remove('openNav');
 });
+
+
+const moreIngredientesBtn = document.querySelector(".more-ingredients-btn");
+const moreInstructionsBtn = document.querySelector(".more-instructions-btn");
+const addCategoryBtn = document.querySelector(".add-category-btn");
+
+moreIngredientesBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const ingredientsContainer = document.querySelector(".ingredients");
+
+    const inputIngredients = document.createElement('input');
+    inputIngredients.type = "text";
+    inputIngredients.name = "recipeingredientes";
+
+    ingredientsContainer.appendChild(inputIngredients);
+});
+
+moreInstructionsBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const instructionsContainer = document.querySelector(".instructions")
+
+    const inputInstructions = document.createElement('input');
+    inputInstructions.type = "text";
+    inputInstructions.name = "recipeinstructions";
+
+    instructionsContainer.appendChild(inputInstructions);
+});
+
+addCategoryBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const categoriesContainer = document.querySelector(".categories")
+
+    const inputCategories = document.createElement('input');
+    inputCategories.type = "text";
+    inputCategories.name = "recipecategories";
+
+    categoriesContainer.appendChild(inputCategories);
+});
