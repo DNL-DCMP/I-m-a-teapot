4acd3aa7b49a9dce406a68cf437b8eadb11c8cb6
DNL_DCMP <ddocampo@fi.uba.ar>
2024-12-15 19:00:55 -0300

    Agrego put y get de comentarios

--- backend/src/routes/comments.js
+++ backend/src/routes/comments.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+/* Muestra todos los comentarios de una receta */
+router.get('/:id/comments', async (req, res) => {
+    const recipe = await prisma.recipe.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        },
+        include: {
+            comments: true
+        }
+    })
+
+    if(recipe === null){
+        res.sendStatus(404)
+        return
+    }
+
+    res.send(recipe.comments)
+})
 
 /* Agregar comentario a una receta */
 router.post('/:id/comments', async (req, res) => {
@@ -28,6 +46,31 @@ router.post('/:id/comments', async (req, res) => {
     })
 
     res.status(201).send(comment)
+})
+
+/* Modificar un comentario de una receta */
+router.put('/:id/comments/:commentId', async (req, res) => {
+    const comment = await prisma.comment.findUnique({
+        where: {
+            id: parseInt(req.params.commentId)
+        }
+    })
+
+    if(comment === null){
+        res.sendStatus(404)
+        return
+    }
+
+    const updatedComment = await prisma.comment.update({
+        where: {
+            id: parseInt(req.params.commentId)
+        },
+        data: {
+            content: req.body.content
+        }
+    })
+
+    res.send(updatedComment)
 })
 
 /* Eliminar un comentario de una receta*/
