6fb6f19328b3b8cbab049a8c3834b7dff5628fff
Ailin Sofía <afalcon@fi.uba.ar>
2024-12-16 11:36:48 -0300

    Agrego función para mostrar las recetas del usuario

--- frontend/user-recipes.js
+++ frontend/user-recipes.js
@@ -1,3 +1,5 @@
+const { link } = require("fs");
+
 let ordenAscendente = true; 
 
 function ordenarTarjetas() {
@@ -24,3 +26,45 @@ function ordenarTarjetas() {
 
     ordenAscendente = !ordenAscendente;
 }
+
+const recipesContainer = document.querySelector('.container-recipes');
+
+/*Falta agregar el id del usuario logueado*/
+
+fetch("http://localhost:3000/ap1/v1/users/:id/recipes")
+    .then(response => response.json())
+    .then(recipes => {
+        recipes.forEach((recipe) => {
+            let div = document.createElement('div');
+            div.classList.add('crad-recipe');
+
+            /*Hay que agregarlo a schema.prisma y el endpoint de recipes*/
+            let img = document.createElement('img');
+            img.src = recipe.image;
+
+            /* Titulo de la receta */
+            let titulo = document.createElement('h3');
+            titulo.classList.add('recipe-name');
+            titulo.innerText = recipe.name;
+
+            /*Descripcion de la receta */
+            let desc = document.createElement('p');
+            desc.classList.add('recipe-description');
+            desc.innerText = recipe.description;
+
+            /* Boton de ver receta */
+            let linkViewRecipe = document.createElement('a');
+            linkViewRecipe.classList.add('view-recipe-btn');
+            linkViewRecipe.innerText = "Ver receta";
+            linkViewRecipe.href = `recipe-details.html?id=${recipe.id}`;
+
+            div.appendChild(img);
+            div.appendChild(titulo);
+            div.appendChild(desc);
+            div.appendChild(linkViewRecipe);
+
+            recipesContainer.appendChild(div);
+
+        })
+    })
+
