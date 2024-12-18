9f2f054aec82fa4ad32cce6ca41d2a714faaed9c
Mateo <moserrano@fi.uba.ar>
2024-12-18 11:04:54 +0000

    Agrego campos a recipe

---
+++ backend/prisma/migrations/20241218110151_agrego_campos_a_recipe/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `servings` to the `Recipe` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `temperatureCook` to the `Recipe` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `time` to the `Recipe` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Recipe" ADD COLUMN     "servings" INTEGER NOT NULL,
+ADD COLUMN     "temperatureCook" INTEGER NOT NULL,
+ADD COLUMN     "time" INTEGER NOT NULL;
