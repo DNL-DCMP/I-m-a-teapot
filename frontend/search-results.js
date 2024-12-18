253c3c71f2c9f51a344f9c6fb867cd5df411774a
Ailin Sofía <afalcon@fi.uba.ar>
2024-12-18 17:56:16 -0300

    Arreglo errores en css de search-results.html

--- frontend/search-results.js
+++ frontend/search-results.js
@@ -4,14 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (searchResults && searchResults.length > 0) {
         searchResults.forEach(recipe => {
-            const recipeElement = document.createElement('div')
-            recipeElement.className = 'recipe'
-            recipeElement.innerHTML = `
-                <h2>${recipe.name}</h2>
-                <p>${recipe.description}</p>
-            `
-            resultsContainer.appendChild(recipeElement)
+                let div = document.createElement('div');
+                div.classList.add('card-recipe');
+
+                let img = document.createElement('img');
+                img.src = recipe.image;
+
+                /* Titulo de la receta */
+                let titulo = document.createElement('h3');
+                titulo.classList.add('recipe-name');
+                titulo.innerText = recipe.name;
+
+                /*Descripcion de la receta */
+                let desc = document.createElement('p');
+                desc.classList.add('recipe-description');
+                desc.innerText = recipe.description;
+
+                /* Boton de ver receta */
+                let linkViewRecipe = document.createElement('a');
+                linkViewRecipe.classList.add('view-recipe-btn');
+                linkViewRecipe.innerText = "Ver receta";
+                linkViewRecipe.href = `recipes-details.html?id=${recipe.id}`;
+
+                /*div.appendChild(img);*/
+                div.appendChild(titulo);
+                div.appendChild(desc);
+                div.appendChild(linkViewRecipe);
+
+                resultsContainer.appendChild(div);
         })
+
     } else {
         resultsContainer.innerHTML = '<p>No se encontraron recetas.</p>'
     }
