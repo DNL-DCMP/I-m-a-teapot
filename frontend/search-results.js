e81e3b6ad52271a6c241fe8ec351d73376eded24
Ailin Sofía <afalcon@fi.uba.ar>
2024-12-19 10:11:55 -0300

    Arreglo side bar

--- frontend/search-results.js
+++ frontend/search-results.js
@@ -1,3 +1,32 @@
+const nav = document.querySelector(".nav");
+const searchIcon = document.querySelector("#searchIcon");
+const navOpenBtn = document.querySelector(".navOpenBtn");
+const navCloseBtn = document.querySelector(".navCloseBtn");
+
+
+/*Agrega funcionalidad al boton de busqueda */
+searchIcon.addEventListener('click', () => {
+    nav.classList.toggle('openSearch');
+    nav.classList.remove('openNav');
+
+    if(nav.classList.contains('openSearch')){
+       return searchIcon.classList.replace("bx-search", "bx-x");
+    }
+
+    searchIcon.classList.replace("bx-x", "bx-search");
+});
+
+/*Agrega funcionalidad al boto de menu y cierre*/
+navOpenBtn.addEventListener('click', () => {
+    nav.classList.add('openNav');
+    nav.classList.remove('openSearch');
+    searchIcon.classList.replace("bx-x", "bx-search");
+});
+
+navCloseBtn.addEventListener('click', () => {
+    nav.classList.remove('openNav');
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const resultsContainer = document.getElementById('results')
     const searchResults = JSON.parse(localStorage.getItem('searchResults'))
