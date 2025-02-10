8981664f50b0af9e81b735ccb907831a8e1e251e
Ailin Sofía <afalcon@fi.uba.ar>
2025-02-10 13:05:26 -0300

    Arreglo errores de diseño en el perfil del usuario

--- frontend/user-profile.js
+++ frontend/user-profile.js
@@ -1,35 +1,3 @@
-
-//  import { clear } from "console";
-//  import { use } from "../backend/src/routes/users";
-
-const nav = document.querySelector(".nav");
-const searchIcon = document.querySelector("#searchIcon");
-const navOpenBtn = document.querySelector(".navOpenBtn");
-const navCloseBtn = document.querySelector(".navCloseBtn");
-
-/*Agrega funcionalidad al boton de busqueda */
-searchIcon.addEventListener('click', () => {
-    nav.classList.toggle('openSearch');
-    nav.classList.remove('openNav');
-
-    if(nav.classList.contains('openSearch')){
-       return searchIcon.classList.replace("bx-search", "bx-x");
-    }
-
-    searchIcon.classList.replace("bx-x", "bx-search");
-});
-
-/*Agrega funcionalidad al boto de menu y cierre*/
-navOpenBtn.addEventListener('click', () => {
-    nav.classList.add('openNav');
-    nav.classList.remove('openSearch');
-    searchIcon.classList.replace("bx-x", "bx-search");
-});
-
-navCloseBtn.addEventListener('click', () => {
-    nav.classList.remove('openNav');
-});
-
 const user = JSON.parse(localStorage.getItem('user'));
 
 if(user){
