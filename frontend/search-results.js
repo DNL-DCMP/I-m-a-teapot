2cfbf5606300744b8a431ad4ba7dac8b9fe97b2d
DNL_DCMP <ddocampo@fi.uba.ar>
2025-02-18 17:49:53 -0300

    Agrego el script de search-recipes.js a todos los html necesarios para que se pueda buscar desde cualquiera de ellos y no solo desde inicio

--- frontend/search-results.js
+++ frontend/search-results.js
@@ -1,3 +1,53 @@
+const nav = document.querySelector(".nav");
+const searchIcon = document.querySelector("#searchIcon");
+const navOpenBtn = document.querySelector(".navOpenBtn");
+const navCloseBtn = document.querySelector(".navCloseBtn");
+
+/*Agrega funcionalidad al boton de busqueda */
+searchIcon.addEventListener('click', () => {
+    nav.classList.toggle('openSearch');
+    nav.classList.remove('openNav');
+
+    if(nav.classList.contains('openSearch')){
+       return searchIcon.classList.replace("bx-search", "bx-x");
+    }
+
+    searchIcon.classList.replace("bx-x", "bx-search");
+});
+
+/*Agrega funcionalidad al boton de menu y cierre*/
+navOpenBtn.addEventListener('click', () => {
+    nav.classList.add('openNav');
+    nav.classList.remove('openSearch');
+    searchIcon.classList.replace("bx-x", "bx-search");
+});
+
+navCloseBtn.addEventListener('click', () => {
+    nav.classList.remove('openNav');
+});
+
+
+
+/* Autentication */
+document.addEventListener("DOMContentLoaded", () => {
+    // Obtener el usuario desde localStorage
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    // Verificar si el usuario existe y si está logueado
+    if (user && user.isLoggedIn) {
+        console.log("El usuario está logueado:", user.isLoggedIn);
+
+        const authButtons = document.querySelectorAll(".auth");
+        authButtons.forEach(button => button.classList.add("hidden"));
+        
+    } else {
+        console.log("El usuario no está logueado o no existe.");
+        const notAuthButtons = document.querySelectorAll(".notAuth");
+        notAuthButtons.forEach(button => button.classList.add("hidden") )
+    }
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const resultsContainer = document.getElementById('results');
     function getCookie(name) {
