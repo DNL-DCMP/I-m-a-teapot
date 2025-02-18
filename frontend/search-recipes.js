2af53233eb68aaf0b6c366625969d48b69c9a5a3
DNL_DCMP <ddocampo@fi.uba.ar>
2025-02-18 19:35:32 -0300

    Actualizo la función de búsqueda para utilizar localStorage en lugar de cookies por conflictos con las imagenes

--- frontend/search-recipes.js
+++ frontend/search-recipes.js
@@ -2,19 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchIcon = document.getElementById('search-box-icon');
     const searchInput = document.querySelector('.search-box-input');
 
-    function setCookie(name, value, maxAge) {
-        document.cookie = `${name}=${encodeURIComponent(value)}; path=/; max-age=${maxAge}`;
-    }
-
     searchIcon.addEventListener('click', async () => {
         const query = searchInput.value.toLowerCase();
         if (query) {
-            try {
-                const response = await fetch('http://localhost:3000/api/v1/recipes');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const data = await response.json();
+            fetch ('http://localhost:3000/api/v1/recipes')
+            .then(response => response.json())
+            .then(data => {
                 // Filtrar las recetas según el término de búsqueda
                 const filteredRecipes = data.filter(recipe => 
                     recipe.name.toLowerCase().includes(query) ||
@@ -22,13 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     recipe.categories.some(category => category.toLowerCase().includes(query))
                 );
                 
-                // Almacenar los resultados en una cookie para acceder a ellos en la página de resultados
-                setCookie('searchResults', JSON.stringify(filteredRecipes), 3600); // Expira en 1 hora
+                // Almacenar los resultados en una localStorage para acceder a ellos en la página de resultados
+                localStorage.setItem('searchResults', JSON.stringify(filteredRecipes));
                 window.location.href = 'search-results.html';
-            } catch (error) {
-                console.error('Error fetching search results:', error);
-                alert('Hubo un problema al obtener los resultados de búsqueda. Por favor, inténtelo de nuevo.');
-            }
+            });
         } else {
             alert('Por favor, ingrese un término de búsqueda.');
         }
--- frontend/search-results.js
+++ frontend/search-results.js
@@ -50,54 +50,58 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const resultsContainer = document.getElementById('results');
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
+    const searchResults = JSON.parse(localStorage.getItem('searchResults')) || [];
+
+    // function getCookie(name) {
+    //     const value = `; ${document.cookie}`;
+    //     const parts = value.split(`; ${name}=`);
+    //     if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
+    // }
+
+    // function deleteCookie(name) {
+    //     document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    // }
+
+    // const searchResults = JSON.parse(getCookie('searchResults'));
+
+    // deleteCookie('searchResults');
+    if (searchResults.length === 0) {
+        resultsContainer.innerHTML = '<p>No se encontraron recetas</p>';
+        return;
     }
 
-    function deleteCookie(name) {
-        document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
-    }
+     
+    searchResults.forEach(recipe => {
+        let div = document.createElement('div');
+        div.classList.add('card-recipe');
 
-    const searchResults = JSON.parse(getCookie('searchResults'));
+        let img = document.createElement('img');
+        img.src = recipe.recipePicture;
 
-    deleteCookie('searchResults');
+        /* Titulo de la receta */
+        let titulo = document.createElement('h3');
+        titulo.classList.add('recipe-name');
+        titulo.innerText = recipe.name;
 
-    if (searchResults && searchResults.length > 0) {
-        searchResults.forEach(recipe => {
-            let div = document.createElement('div');
-            div.classList.add('card-recipe');
+        /* Descripcion de la receta */
+        let desc = document.createElement('p');
+        desc.classList.add('recipe-description');
+        desc.innerText = recipe.description;
 
-            let img = document.createElement('img');
-            img.src = recipe.recipePicture;
-
-            /* Titulo de la receta */
-            let titulo = document.createElement('h3');
-            titulo.classList.add('recipe-name');
-            titulo.innerText = recipe.name;
-
-            /* Descripcion de la receta */
-            let desc = document.createElement('p');
-            desc.classList.add('recipe-description');
-            desc.innerText = recipe.description;
-
-            /* Boton de ver receta */
-            let linkViewRecipe = document.createElement('a');
-            linkViewRecipe.classList.add('view-recipe-btn');
-            linkViewRecipe.innerText = "Ver receta";
-            linkViewRecipe.addEventListener(`click`, () => {
-                document.cookie = `recipeid=${recipe.id}; path=/; max-age=3600`; // Expira en 1 hora
-                window.location.href = 'recipes-details.html';
-            });
-            div.appendChild(img);
-            div.appendChild(titulo);
-            div.appendChild(desc);
-            div.appendChild(linkViewRecipe);
-
-            resultsContainer.appendChild(div);
+        /* Boton de ver receta */
+        let linkViewRecipe = document.createElement('a');
+        linkViewRecipe.classList.add('view-recipe-btn');
+        linkViewRecipe.innerText = "Ver receta";
+        linkViewRecipe.addEventListener(`click`, () => {
+            document.cookie = `recipeid=${recipe.id}; path=/; max-age=3600`; // Expira en 1 hora
+            window.location.href = 'recipes-details.html';
         });
-    } else {
-        resultsContainer.innerHTML = '<p>No se encontraron recetas.</p>';
-    }
+        div.appendChild(img);
+        div.appendChild(titulo);
+        div.appendChild(desc);
+        div.appendChild(linkViewRecipe);
+
+        resultsContainer.appendChild(div);
+    });
+
 });
